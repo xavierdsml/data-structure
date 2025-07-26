57a96b9b390b1c4852d87870eaf218597a578d30
Mr. Xavier <153775246+xavierdsml@users.noreply.github.com>
2025-07-26 14:23:10 +0530

    Leetcode : 72

---
+++ Dynamic Programming/DP on Strings/Edit Distance.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<int> prev(m+1, 0), curr(m+1, 0);
+
+        for(int j = 0; j<=m; j++) prev[j] = j;
+
+        for(int i = 1; i<=n; i++){
+            curr[0] = i;
+            for(int j = 1; j<=m; j++){
+                if(word1[i-1] == word2[j-1]) curr[j] = prev[j-1];
+                else curr[j] = 1 + min(curr[j-1], min(prev[j], prev[j-1]));
+            }
+            prev = curr;
+        }
+
+        return prev[m];
+    }
+};
