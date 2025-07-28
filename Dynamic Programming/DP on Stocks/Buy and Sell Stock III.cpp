5d360db64771dc295ec17cf9d085de6ab0c3ea10
Mr. Xavier <153775246+xavierdsml@users.noreply.github.com>
2025-07-28 17:30:38 +0530

    Leetcode : 123

---
+++ Dynamic Programming/DP on Stocks/Buy and Sell Stock III.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>>after(2, vector<int>(3, 0));
+        vector<vector<int>>curr(2, vector<int>(3, 0));
+        
+        for(int ind = prices.size()-1; ind>=0; ind--){
+            for(int buy = 0; buy<=1; buy++){
+                for(int cap = 1; cap<=2; cap++){
+
+                    int profit = 0;
+                    if(buy){
+                        profit = max(-prices[ind] + after[0][cap],
+                        0 + after[1][cap]);
+                    }
+                    else{
+                        profit = max(prices[ind] + after[1][cap-1],
+                        0 + after[0][cap]);
+                    }
+
+                    curr[buy][cap] = profit;
+                }
+            }
+            after = curr;
+        }
+        return after[1][2];
+    }
+};
